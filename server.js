18c231f9014218880a534d55810d07264a09c7fa
Maria Thomasson <81348201+MariaThomasson@users.noreply.github.com>
2021-12-08 12:37:01 +0100

    Update after wednesday lecture

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import listEndpoints from 'express-list-endpoints'
 import cors from 'cors'
 import techFundings from './data/tech_fundings.json'
 
@@ -19,6 +20,10 @@ app.get('/', (req, res) => {
  res.send('Hello from us!')
 })
 
+app.get('endpoints', (req, res) => {
+ res.send(listEndpoints(app))
+})
+
 app.get('/users', (req, res) => {
   const users = [
   {id: 1, name: 'Alice', age: 33},
@@ -30,11 +35,31 @@ res.json(users)
 })
 
 app.get('/fundings', (req, res) => {
-  res.json(techFundings)
+const { company, region } = req.query
+
+let techFundingsToSend = techFundings
+
+if (company) {
+  techFundingsToSend = techFundingsToSend.filter(
+  (item) => item.Company.toLowerCase().indexOf(company.toLowerCase()) !== -1
+  )
+}
+
+if (region) {
+  techFundingsToSend = techFundingsToSend.filter(
+    (item) => item.Region.toLowerCase().indexOf(region.toLowerCase()) !== -1
+  )
+}
+
+  res.json({
+    response: techFundingsToSend,
+    success: true,
+  })
 })
 
+
 // get a specific company based on id(index), using param
-app.get('/fundings/:index', (req, res) => {
+app.get('/fundings/index/:index', (req, res) => {
   const { index } = req.params
 
   const companyId = techFundings.find(company => company.index === +index)
@@ -45,6 +70,24 @@ app.get('/fundings/:index', (req, res) => {
   } else {
     res.json(companyId)
   }
+})
+
+app.get('/fundings/company/:company', (req, res) => {
+  const { company } = req.params
+
+  const companyByName = techFundings.find(item => item.Company === company)
+
+  if (!companyByName) {
+    res.status(404).json({
+      response: 'No company found with that name',
+      success: false
+    })
+  
+  } else
+  res.status(200).json({
+    response: companyByName,
+    success: true
+  })
 })
 
 // Start defining your routes here
